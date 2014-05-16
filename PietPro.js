26fdc048144d23a13b369f8eec13e2c9afc19928
Matthew Kaufer <mjkaufer@gmail.com>
2014-05-16 15:15:00 -0400

    Functions!
    Change in function changes color
    
    Todo: Resizing & exporting

--- PietPro.js
+++ PietPro.js
@@ -7,7 +7,7 @@ if (Meteor.isClient) {
     ['#C0FFC0', '#00FF00', '#00C000'],
     ['#C0FFFF', '#00FFFF', '#00C0C0'],
     ['#C0C0FF', '#0000FF', '#0000C0'],        
-    ['#FFC0FF', '#FF00FF', '#C000C0'],    
+    ['#FFC0FF', '#FF00FF', '#C000C0'],   //rendered sideways 
   ];
 
 var commandArr = [
@@ -19,10 +19,27 @@ var commandArr = [
     ['in(char)', 'out(num)', 'out(char)'],    
   ];  
 
-  Session.set('currentCol', '#FFFFFF');
+  function arrFind(arr, target){//returns [row, col]
+    for(var x = 0; x < arr.length; x++)
+      if(arr[x].indexOf(target) > -1)
+        return [x, arr[x].indexOf(target)];
+    return [-1, -1];
+  }
+
+  Session.set('currentCol', '#FFC0C0');
+  Session.set('rows', 12);
+  Session.set('cols', 4);
+  Template.main.rows = function(){
+    return Session.get('rows');
+  }
+
+  Template.main.cols = function(){
+    return Session.get('cols');
+  }
 
   Template.main.rowCol = function(row, col, cl){
-
+    row = parseInt(row);
+    col = parseInt(col);//just in case strings
     var ret = "<table class=" + cl + ">\n<tbody>\n";
     for(var r = 0; r < row; r++){
       ret+="<tr>\n";
@@ -46,22 +63,106 @@ var commandArr = [
     return ret + "</tbody>\n</table>";
   }
 
+  Template.main.printFunctions = function(){
+    var ret = "<table class='fcell'>\n<tbody>\n";
+    for(var c = 0; c < commandArr[0].length; c++){
+      ret+="<tr>\n";
+      for(var r = 0; r < commandArr.length; r++){
+        ret+="<td class='fcell'>" + commandArr[r][c] + "</td>\n";
+      }
+      ret+="</tr>\n";
+    }
+    return ret + "</tbody>\n</table>";
+  }
+
+  Template.main.rendered = function(){
+    $('.ccell')[1].className+=" selected";//makes first color thing selected
+  }
+
   Template.main.events({
-    'click td.cell': function (event) {
+    'click td.cell': function(event){
       // template data, if any, is available in 'this'
       console.log(event);
       event.target.style.backgroundColor = Session.get('currentCol');
     },
-    'click td.ccell': function (event) {
+    'click td.ccell': function(event){
       // template data, if any, is available in 'this'
-      Session.set('currentCol', event.target.style.backgroundColor);
-      
+      console.log('ABCD');
+      console.log(event.target.style.backgroundColor)
+      console.log(typeof event.target.style.backgroundColor)
+      console.log(rgbToHex(event.target.style.backgroundColor));
+      Session.set('currentCol', rgbToHex(event.target.style.backgroundColor));
+
       for(var x = 0; x < $('.selected').length; x++)
         $('.selected')[x].className = "ccell";
 
       event.target.className += " selected";
-    }  
+    },
+    'click td.fcell': function(event){
+      var coords = arrFind(commandArr, event.target.innerHTML);
+      console.log("Func coords");
+      console.log(coords);
+      if(coords[0] == 0 && coords[1] == 0)//&nbsp;
+        return;
+
+      colCoords = arrFind(colArr, Session.get('currentCol'));
+      var r = (colCoords[0] + coords[0]) % colArr.length;
+      var c = (colCoords[1] + coords[1]) % colArr[0].length;
+
+      var newCol = colArr[r][c];
+      console.log(r + ":" + c + ":" + newCol);
+      console.log((r + colArr.length * c + 1));
+      console.log('abcdefg123');
+      // console.log(colCoords);
+      // console.log(coords);
+      // console.log('col & coords above');
+      // console.log(newCol);
+      Session.set('currentCol', newCol);
+      // console.log(newCol);
+      setSelected(newCol, (r + colArr.length * c + 1));
+
+
+    }
   });
+
+  function setSelected(colCode, ind){//includes +1
+    for(var x = 0; x < $('.selected').length; x++)
+      $('.selected')[x].className = "ccell";
+
+    // var coords = arrFind(colArr, colCode);
+    // console.log('coords')
+    // console.log(coords);
+
+    // var add = 1 + coords[1] column+ colArr[0].length * coords[0];
+    // console.log(add);
+    // console.log('abcdefg');
+    
+    // $('.ccell')[add].className+=" selected";
+    $('.ccell')[ind].className+= " selected";
+
+  }
+
+  function rgbToHex(c){
+    //c is, for instance, "rgb(192, 255, 255)"
+    var arr = c.replace("rgb(","").replace(")","").split(", ");
+    console.log(arr);
+    for(var x = 0; x < arr.length; x++)
+      arr[x] = parseInt(arr[x])
+    return rgbFin(arr);
+  }
+
+
+  function componentToHex(c) {
+      var hex = c.toString(16).toUpperCase();
+      return hex.length == 1 ? "0" + hex : hex;
+  }
+
+  function rgbFin(arr) {
+      return "#" + componentToHex(arr[0]) + componentToHex(arr[1]) + componentToHex(arr[2]);
+  }
+
+
+
 
 }
 
